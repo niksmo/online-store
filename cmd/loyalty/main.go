b0731bdb5ea5561f6b104d7e93717e0156cf5ca2
nikolay <n.smolov@yandex.ru>
2025-03-30 20:15:07 +0300

    prepare loyalty and dashboard services

--- cmd/loyalty/main.go
+++ cmd/loyalty/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"context"
+	"niksmo/online-store/pkg/logger"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	stopCtx, stopFn := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stopFn()
+
+	logger.Init()
+
+	<-stopCtx.Done()
+	logger.Instance.Info().Msg("gracefully shutdown")
+}
